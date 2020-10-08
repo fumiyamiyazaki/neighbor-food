bcd37c9183ef6f9b28b6bd692b51717ebeb8adcc
fumiyamiyazaki <fmy9611@icloud.com>
2020-10-09 02:23:28 +0900

    set

---
+++ views/index.php
@@ -0,0 +1,24 @@
+
+
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>my_app</title>
+<link rel="stylesheet" type="text/css" href="../css/reset.css">
+<link rel="stylesheet" type="text/css" href="../css/index.css">
+<script type="text/javascript" src="js/jquery.js"></script>
+<script>
+</script>
+</head>
+<body>
+
+
+  <div>
+    
+  </div>
+  
+
+</body>
+</html>

e8690f8a1f204219e6c983952891d7d6f65aac34
fumiyamiyazaki <fmy9611@icloud.com>
2020-10-10 01:43:09 +0900

    views

--- views/index.php
+++ views/index.php
@@ -4,21 +4,45 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-<meta charset="utf-8">
+<meta charset="UTF-8">
 <title>my_app</title>
 <link rel="stylesheet" type="text/css" href="../css/reset.css">
 <link rel="stylesheet" type="text/css" href="../css/index.css">
-<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="../js/jquery.js"></script>
 <script>
 </script>
 </head>
 <body>
 
 
-  <div>
-    
+
+
+  <section>
+  <div class="main_content">
+
+    <div class="main_left">
+      <h1>neighbor - <span>Food</span></h1>
+      <p class="about_p">今すぐ行けるラーメン屋を探せる</p>
+      <p class="about_text">即行けラーメン検索プラットフォーム</p>
+    </div>
+
+    <div class="main_right">
+      <img src="../img/main.jpg" alt="メインの画像" class="right_img">
+    </div>
+
   </div>
-  
+  </section>
+
+  <section>
+  <div class="introduction">
+    <div class="head_int">
+      <img src="../img/flame.png" alt="暖簾の画像" class="flame_img">
+      <p class="head_p">introduction</p>
+    </div>
+
+  </div>
+  </section>
+
 
 </body>
 </html>
---
+++ views/shared/_header.php
@@ -0,0 +1,24 @@
+<div class="shared_header">
+
+  <div class="header_title">
+    <p><a href="/my_app/views/index.php">neighbor - <span>Food</span></a></p>
+
+  </div>
+
+  <div class="header_nav">
+    <li>
+      <ul>
+        <a href="#">a</a>
+      </ul>
+      <ul>
+        <a href="#">b</a>
+      </ul>
+      <ul>
+        <a href="#">c</a>
+      </ul>
+    </li>
+  </div>
+
+</div>
+
+<div class="content_wrapp">

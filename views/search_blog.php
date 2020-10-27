60dc46b0178f6f8d630d854694672938ee13d987
fumiyamiyazaki <fmy9611@icloud.com>
2020-10-28 02:24:31 +0900

    scroll_btn

--- views/search_blog.php
+++ views/search_blog.php
@@ -16,13 +16,29 @@
 <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
 <script type="text/javascript" src="../js/jquery.js"></script>
 <script>
+
+$(function() {
+  scrollTop();
+});
+
+
+  function scrollTop() {
+    $('a[href^=#]').click(function(){
+      var speed = 500;
+      var href = $(this).attr('href');
+      var target = $(href == '#' || href == '' ? 'html':href);
+      var position = target.offset().top;
+      $('body,html').animate({scrollTop:position},speed,'swing')
+      return false;
+    });
+  }
 </script>
 </head>
 <body>
 
   <div class="search_wrapp">
 
-    <div class="search_header">
+    <div class="search_header" id="top_page">
       <img src="../img/search.png" alt="ラーメンの画像">
       <h1>stores-blog</h1>
 
@@ -40,7 +56,7 @@
       <!-- for分で回す -->
       <div class="blog_content">
         <div class="blog_img-wrapp">
-          <img src="" alt="">
+          <img src="../img/main.jpg" class="blog_img-pict" alt="ブログの写真">
         </div>
         <div class="blog_info-wrapp">
           <p class="store_name-blog">aaaaa</p>
@@ -50,10 +66,127 @@
         <p class="sentence_blog">あああああああああああああああああああああ...</p>
       </div>
       <!--  -->
+      <div class="blog_content">
+        <div class="blog_img-wrapp">
+          <img src="../img/main.jpg" class="blog_img-pict" alt="ブログの写真">
+        </div>
+        <div class="blog_info-wrapp">
+          <p class="store_name-blog">aaaaa</p>
+          <p class="created_blog">2020-10-27</p>
+        </div>
+        <!-- 21文字まで -->
+        <p class="sentence_blog">あああああああああああああああああああああ...</p>
+      </div>
+      <div class="blog_content">
+        <div class="blog_img-wrapp">
+          <img src="../img/main.jpg" class="blog_img-pict" alt="ブログの写真">
+        </div>
+        <div class="blog_info-wrapp">
+          <p class="store_name-blog">aaaaa</p>
+          <p class="created_blog">2020-10-27</p>
+        </div>
+        <!-- 21文字まで -->
+        <p class="sentence_blog">あああああああああああああああああああああ...</p>
+      </div>
+      <div class="blog_content">
+        <div class="blog_img-wrapp">
+          <img src="../img/main.jpg" class="blog_img-pict" alt="ブログの写真">
+        </div>
+        <div class="blog_info-wrapp">
+          <p class="store_name-blog">aaaaa</p>
+          <p class="created_blog">2020-10-27</p>
+        </div>
+        <!-- 21文字まで -->
+        <p class="sentence_blog">あああああああああああああああああああああ...</p>
+      </div>
+      <div class="blog_content">
+        <div class="blog_img-wrapp">
+          <img src="../img/main.jpg" class="blog_img-pict" alt="ブログの写真">
+        </div>
+        <div class="blog_info-wrapp">
+          <p class="store_name-blog">aaaaa</p>
+          <p class="created_blog">2020-10-27</p>
+        </div>
+        <!-- 21文字まで -->
+        <p class="sentence_blog">あああああああああああああああああああああ...</p>
+      </div>
+      <div class="blog_content">
+        <div class="blog_img-wrapp">
+          <img src="../img/main.jpg" class="blog_img-pict" alt="ブログの写真">
+        </div>
+        <div class="blog_info-wrapp">
+          <p class="store_name-blog">aaaaa</p>
+          <p class="created_blog">2020-10-27</p>
+        </div>
+        <!-- 21文字まで -->
+        <p class="sentence_blog">あああああああああああああああああああああ...</p>
+      </div>
+      <div class="blog_content">
+        <div class="blog_img-wrapp">
+          <img src="../img/main.jpg" class="blog_img-pict" alt="ブログの写真">
+        </div>
+        <div class="blog_info-wrapp">
+          <p class="store_name-blog">aaaaa</p>
+          <p class="created_blog">2020-10-27</p>
+        </div>
+        <!-- 21文字まで -->
+        <p class="sentence_blog">あああああああああああああああああああああ...</p>
+      </div>
+      <div class="blog_content">
+        <div class="blog_img-wrapp">
+          <img src="../img/main.jpg" class="blog_img-pict" alt="ブログの写真">
+        </div>
+        <div class="blog_info-wrapp">
+          <p class="store_name-blog">aaaaa</p>
+          <p class="created_blog">2020-10-27</p>
+        </div>
+        <!-- 21文字まで -->
+        <p class="sentence_blog">あああああああああああああああああああああ...</p>
+      </div>
+      <div class="blog_content">
+        <div class="blog_img-wrapp">
+          <img src="../img/main.jpg" class="blog_img-pict" alt="ブログの写真">
+        </div>
+        <div class="blog_info-wrapp">
+          <p class="store_name-blog">aaaaa</p>
+          <p class="created_blog">2020-10-27</p>
+        </div>
+        <!-- 21文字まで -->
+        <p class="sentence_blog">あああああああああああああああああああああ...</p>
+      </div>
+      <div class="blog_content">
+        <div class="blog_img-wrapp">
+          <img src="../img/main.jpg" class="blog_img-pict" alt="ブログの写真">
+        </div>
+        <div class="blog_info-wrapp">
+          <p class="store_name-blog">aaaaa</p>
+          <p class="created_blog">2020-10-27</p>
+        </div>
+        <!-- 21文字まで -->
+        <p class="sentence_blog">あああああああああああああああああああああ...</p>
+      </div>
+      <div class="blog_content">
+        <div class="blog_img-wrapp">
+          <img src="../img/main.jpg" class="blog_img-pict" alt="ブログの写真">
+        </div>
+        <div class="blog_info-wrapp">
+          <p class="store_name-blog">aaaaa</p>
+          <p class="created_blog">2020-10-27</p>
+        </div>
+        <!-- 21文字まで -->
+        <p class="sentence_blog">あああああああああああああああああああああ...</p>
+      </div>
 
 
+      <div class="scroll_top-btn">
+        <a href="#top_page"><i class="fas fa-arrow-up top_btn"></i></a>
+      </div>
 
     </div>
+
+    <?php require("shared/_footer.php") ?>
+
+
 
   </div>
 
